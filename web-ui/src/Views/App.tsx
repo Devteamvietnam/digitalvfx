0af0fc90712c9d6592a2ff7a882dfb3d9fbf52ac
ddthien-coder <jungjihoonkr97@gmail.com>
2021-11-19 19:57:08 +0700

    update

---
+++ web-ui/src/Views/App.tsx
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+export class App extends Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      name: "Digital"
+    };
+    
+  }
+  handleChangeName(event: any) {
+    this.setState({ name: event.target.value });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <input type="text" value={this.state.name} onChange={this.handleChangeName.bind(this)} />
+          <p>
+            {this.state.name} <code>VFX</code> Project
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
