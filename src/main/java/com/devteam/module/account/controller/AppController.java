9f84d794e5b910ab2a7437ab48a115ac9094061f
ddthien-coder <jungjihoonkr97@gmail.com>
2021-11-20 17:01:52 +0700

    update

---
+++ src/main/java/com/devteam/module/account/controller/AppController.java
@@ -0,0 +1,42 @@
+package com.devteam.module.account.controller;
+
+import com.devteam.config.GeneralConfig;
+import com.devteam.module.account.service.AccountService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/")
+public class AppController {
+
+    private static final Logger log = LoggerFactory.getLogger(AppController.class);
+
+
+    public static String applicationInstanceName = "Digital back-end API";
+
+    @Autowired
+    GeneralConfig config;
+    @Autowired
+    AccountService service;
+
+    @PostConstruct
+    public void init() throws JsonProcessingException {
+        log.info("Initialize data");
+
+        service.init();
+
+        if(config.DUMMY_DATA_ENABLED || config.BYPASS_AUTHENTICATION) {
+            service.initDummyData();
+        }
+    }
+
+
+}
